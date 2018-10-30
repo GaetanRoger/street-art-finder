2256edee60d72f38050cb28e70936b8928d1d8fb
Gaetan Roger <gaetanroger.fr@gmail.com>
2018-10-30 15:11:21 +0100

    Decrement score user if deleted piece was found

--- functions/src/firestore/pieces/onDelete.ts
+++ functions/src/firestore/pieces/onDelete.ts
@@ -1,13 +1,14 @@
 import {DocumentSnapshot} from 'firebase-functions/lib/providers/firestore';
 import {EventContext} from 'firebase-functions';
-import * as admin from 'firebase-admin';
 import {Collections} from '../collections.enum';
 import {algolia} from '../../initAlgolia';
 import {getFirestore} from '../../getFirestore';
 import {Helpers} from '../../helpers';
+import QuerySnapshot = FirebaseFirestore.QuerySnapshot;
+import * as admin from 'firebase-admin';
 
 export function firestorePiecesOnDelete(snap: DocumentSnapshot, context: EventContext) {
-    const piece = snap.data() ;
+    const piece = snap.data();
     const id = snap.id;
 
     return Promise.all([
@@ -43,7 +44,7 @@ async function decrementMaxScoreOnUsersArtists(artistId: string, vanished: boole
 function decrementPiecesCountOnArtistDocument(piece) {
     const artist = getFirestore().doc(`${Collections.artists}/${piece.artist.objectID}`);
 
-    return Helpers.increment(artist, 'piecesCount',-1);
+    return Helpers.increment(artist, 'piecesCount', -1);
 }
 
 function deleteAlgoliaObject(id: string) {
@@ -52,18 +53,41 @@ function deleteAlgoliaObject(id: string) {
 }
 
 async function deleteUsersPieces(id: string) {
-    const usersPieces = await admin.firestore()
+    const usersPieces = await getFirestore()
         .collection(Collections.users_pieces)
         .where('piece.objectID', '==', id)
         .get();
 
-    return usersPieces.forEach(userPiece => {
-        return userPiece.ref.delete();
-    });
+    return await Promise.all(usersPieces.docs.map(async userPiece => {
+        const data = userPiece.data();
+        const found = data.found;
+
+        if (found) {
+            await getFirestore().runTransaction(async t => {
+                const query = getFirestore()
+                    .collection(Collections.users_artists)
+                    .where('user', '==', data.user)
+                    .where('artist.objectID', '==', data.artist.objectID);
+
+                const userArtists = await t.get(query);
+
+                if (userArtists.size !== 1) {
+                    console.warn(`FOUND MORE THAN 1 USER ARTIST FOR USER ${data.user} AND ARTIST`, data.artist);
+                    return await Promise.reject(`FOUND MORE THAN 1 USER ARTIST FOR USER ${data.user} AND ARTIST ${data.artist.objectID}`);
+                }
+
+                return await t.update(userArtists.docs[0].ref, {
+                    score: userArtists.docs[0].data().score - 1
+                });
+            });
+        }
+
+        return await userPiece.ref.delete();
+    }));
 }
 
 function decrementPiecesCountInAggregatesDocument() {
     const aggregates = getFirestore().doc(`${Collections.aggregates}/main`);
 
-    return Helpers.increment(aggregates, 'piecesCount',-1);
+    return Helpers.increment(aggregates, 'piecesCount', -1);
 }
