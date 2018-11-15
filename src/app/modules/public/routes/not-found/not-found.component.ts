5597b1533e7d367d21cb750a2c0e7e02589ea213
Gaetan Roger <gaetanroger.fr@gmail.com>
2018-11-15 14:17:41 +0100

    Quick not found page with link back to home

--- src/app/modules/public/routes/not-found/not-found.component.ts
+++ src/app/modules/public/routes/not-found/not-found.component.ts
@@ -5,12 +5,9 @@ import {Component, OnInit} from '@angular/core';
     templateUrl: './not-found.component.html',
     styleUrls: ['./not-found.component.css']
 })
-export class NotFoundComponent implements OnInit {
+export class NotFoundComponent {
 
     constructor() {
-    }
-
-    ngOnInit() {
     }
 
 }
