6ff47a64f8542436b9b061bbd8a85f710ef41d3d
Gaetan Roger <gaetanroger.fr@gmail.com>
2018-11-21 17:20:06 +0100

    Add button to go to users location on map component

--- src/app/modules/shared/components/map/map.component.ts
+++ src/app/modules/shared/components/map/map.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import * as L from 'leaflet';
 import {featureGroup, FeatureGroup, Map, Marker, TileLayer} from 'leaflet';
 import {BehaviorSubject, Observable} from 'rxjs';
 import {MapHelperService} from '../../../core/services/map-helper/map-helper.service';
@@ -6,227 +7,268 @@ import {ObjectIDable} from '../../types/object-idable';
 import {MapElementInput} from './map-element-input';
 import {filter} from 'rxjs/operators';
 import {CoordsConverter} from '../../../core/services/map-helper/coords-converter';
+import {UserGeolocationService} from '../../../core/services/location/geolocation/user-geolocation.service';
+import 'leaflet-easybutton';
 
 const cloneLayer = require('leaflet-clonelayer');
 
 @Component({
-    selector: 'streart-map',
-    templateUrl: './map.component.html',
-    styleUrls: ['./map.component.css']
+  selector: 'streart-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
 })
 export class MapComponent<T extends ObjectIDable> implements OnInit, OnChanges {
 
-    /* *************************************************** *
-     *           _   _        _ _           _              *
-     *      /\  | | | |      (_) |         | |             *
-     *     /  \ | |_| |_ _ __ _| |__  _   _| |_ ___  ___   *
-     *    / /\ \| __| __| '__| | '_ \| | | | __/ _ \/ __|  *
-     *   / ____ \ |_| |_| |  | | |_) | |_| | ||  __/\__ \  *
-     *  /_/    \_\__|\__|_|  |_|_.__/ \__,_|\__\___||___/  *
-     *                                                     *
-     * *************************************************** */
+  /* *************************************************** *
+   *           _   _        _ _           _              *
+   *      /\  | | | |      (_) |         | |             *
+   *     /  \ | |_| |_ _ __ _| |__  _   _| |_ ___  ___   *
+   *    / /\ \| __| __| '__| | '_ \| | | | __/ _ \/ __|  *
+   *   / ____ \ |_| |_| |  | | |_) | |_| | ||  __/\__ \  *
+   *  /_/    \_\__|\__|_|  |_|_.__/ \__,_|\__\___||___/  *
+   *                                                     *
+   * *************************************************** */
 
-    /*
-     *   ___     __   ___
-     *  |_ _|   / /  / _ \
-     *   | |   / /  | (_) |
-     *  |___| /_/    \___/
-     *
-     */
+  /*
+   *   ___     __   ___
+   *  |_ _|   / /  / _ \
+   *   | |   / /  | (_) |
+   *  |___| /_/    \___/
+   *
+   */
 
-    /**
-     * Elements to be displayed on the map.
-     */
-    @Input() elements: MapElementInput[];
+  /**
+   * Elements to be displayed on the map.
+   */
+  @Input() elements: MapElementInput[];
 
-    /**
-     * Layer to show, either marker or circle.
-     */
-    @Input() layerToShow: 'marker' | 'circle' = 'marker';
+  /**
+   * Layer to show, either marker or circle.
+   */
+  @Input() layerToShow: 'marker' | 'circle' = 'marker';
 
-    /**
-     * True to show the user's marker.
-     */
-    @Input() showUserMarker = true;
-    /**
-     * Initial map zoom.
-     * Zoom is used only if one element is provided. If many, map will use fitBounds.
-     */
-    @Input() zoom = 18;
+  /**
+   * True to show the user's marker.
+   */
+  @Input() showUserMarker = true;
+  /**
+   * Initial map zoom.
+   * Zoom is used only if one element is provided. If many, map will use fitBounds.
+   */
+  @Input() zoom = 18;
 
-    @Input() refresh: BehaviorSubject<boolean>;
+  @Input() refresh: BehaviorSubject<boolean>;
 
-    /**
-     * True to use user's locationApproximation setting.
-     */
-    @Input() randomizeCircle = true;
+  /**
+   * True to use user's locationApproximation setting.
+   */
+  @Input() randomizeCircle = true;
 
-
-    /*
-     *   ___      _    _ _
-     *  | _ \_  _| |__| (_)__
-     *  |  _/ || | '_ \ | / _|
-     *  |_|  \_,_|_.__/_|_\__|
-     *
-     */
-
-    /**
-     * Layer containing user's marker.
-     */
-    userLayer$: Observable<Marker>;
+  @Input() showCurrentPositionButton = true;
 
 
-    /**
-     * Layer containing background tiles.
-     */
-    tileLayer: TileLayer;
+  /*
+   *   ___      _    _ _
+   *  | _ \_  _| |__| (_)__
+   *  |  _/ || | '_ \ | / _|
+   *  |_|  \_,_|_.__/_|_\__|
+   *
+   */
+
+  /**
+   * Layer containing user's marker.
+   */
+  userLayer$: Observable<Marker>;
 
 
-    /*
-     *   ___     _          _
-     *  | _ \_ _(_)_ ____ _| |_ ___
-     *  |  _/ '_| \ V / _` |  _/ -_)
-     *  |_| |_| |_|\_/\__,_|\__\___|
-     *
-     */
-
-    /**
-     * Leaflet map objet.
-     */
-    private _leafletMap: Map;
+  /**
+   * Layer containing background tiles.
+   */
+  tileLayer: TileLayer;
 
 
-    /* ***************************************** *
-     *   __  __      _   _               _       *
-     *  |  \/  |    | | | |             | |      *
-     *  | \  / | ___| |_| |__   ___   __| |___   *
-     *  | |\/| |/ _ \ __| '_ \ / _ \ / _` / __|  *
-     *  | |  | |  __/ |_| | | | (_) | (_| \__ \  *
-     *  |_|  |_|\___|\__|_| |_|\___/ \__,_|___/  *
-     *                                           *
-     * ***************************************** */
+  /*
+   *   ___     _          _
+   *  | _ \_ _(_)_ ____ _| |_ ___
+   *  |  _/ '_| \ V / _` |  _/ -_)
+   *  |_| |_| |_|\_/\__,_|\__\___|
+   *
+   */
 
-    /*
-     *   ___      _    _ _
-     *  | _ \_  _| |__| (_)__
-     *  |  _/ || | '_ \ | / _|
-     *  |_|  \_,_|_.__/_|_\__|
-     *
-     */
+  /**
+   * Leaflet map objet.
+   */
+  private _leafletMap: Map;
+  private _currentPositionButton: any;
 
-    constructor(private readonly mapHelper: MapHelperService) {
+
+  /* ***************************************** *
+   *   __  __      _   _               _       *
+   *  |  \/  |    | | | |             | |      *
+   *  | \  / | ___| |_| |__   ___   __| |___   *
+   *  | |\/| |/ _ \ __| '_ \ / _ \ / _` / __|  *
+   *  | |  | |  __/ |_| | | | (_) | (_| \__ \  *
+   *  |_|  |_|\___|\__|_| |_|\___/ \__,_|___/  *
+   *                                           *
+   * ***************************************** */
+
+  /*
+   *   ___      _    _ _
+   *  | _ \_  _| |__| (_)__
+   *  |  _/ || | '_ \ | / _|
+   *  |_|  \_,_|_.__/_|_\__|
+   *
+   */
+
+  constructor(private readonly mapHelper: MapHelperService,
+              private readonly userLocation: UserGeolocationService) {
+  }
+
+  /**
+   * Layer containing elements, either as circles or markers.
+   */
+  get elementsLayer(): FeatureGroup {
+    return this.layerToShow === 'circle'
+      ? this._circlesLayer
+      : this._markersLayer;
+  }
+
+  get map(): Map {
+    return this._leafletMap;
+  }
+
+  get _cleanedElements(): MapElementInput[] {
+    if (!this.elements) {
+      return [];
     }
 
-    /**
-     * Layer containing elements, either as circles or markers.
-     */
-    get elementsLayer(): FeatureGroup {
-        return this.layerToShow === 'circle'
-            ? this._circlesLayer
-            : this._markersLayer;
+    this.elements = this.elements.filter(el => !!el);
+
+    return this.elements;
+  }
+
+  get _markersLayer(): FeatureGroup {
+    return this._createMarkersLayer();
+  }
+
+  get _circlesLayer(): FeatureGroup {
+    return this._createCirclesLayer();
+  }
+
+
+  ngOnInit(): void {
+    this.tileLayer = MapHelperService.tileLayer();
+    this.userLayer$ = this.mapHelper.userMarker();
+    this._currentPositionButton = this._createCurrentPositionButton();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this._leafletMap && this._markersLayer) {
+      this._updateFitBounds();
     }
 
-    get map(): Map {
-        return this._leafletMap;
+    if (this.refresh) {
+      this.refresh
+        .pipe(filter(v => v === true))
+        .subscribe(() => this._refreshMap());
     }
 
-    ngOnInit(): void {
-        this.tileLayer = MapHelperService.tileLayer();
-        this.userLayer$ = this.mapHelper.userMarker();
+    if (changes.showCurrentPositionButton) {
+      this._showOrHideCurrentPositionButton();
     }
+  }
 
-    ngOnChanges(changes: SimpleChanges): void {
-        if (this._leafletMap && this._markersLayer) {
-            this._updateFitBounds();
-        }
+  /**
+   * Method called when map is ready to be used.
+   * @param map Map object ready to be used.
+   */
+  mapReady(map: Map): void {
+    this._leafletMap = map;
 
-        if (this.refresh) {
-            this.refresh
-                .pipe(filter(v => v === true))
-                .subscribe(() => this._refreshMap());
-        }
+    this._updateFitBounds();
+    this._showOrHideCurrentPositionButton();
+  }
+
+  /*
+   *   ___     _          _
+   *  | _ \_ _(_)_ ____ _| |_ ___
+   *  |  _/ '_| \ V / _` |  _/ -_)
+   *  |_| |_| |_|\_/\__,_|\__\___|
+   *
+   */
+
+  private _createCurrentPositionButton() {
+    return L.easyButton(
+      {
+        states: [{
+          stateName: 'default',
+          title: 'Zoom on your location',
+          icon: '<div style="font-size: 150%;display: flex;justify-content: center;align-items: center;">⌾</div>',
+          onClick: (btn, map) => {
+            this.userLocation
+              .currentGeolocation()
+              .subscribe(l => map.panTo(
+                CoordsConverter.geopointToLatLng(l),
+                {animate: true, duration: 1}
+              ));
+          }
+        }]
+      });
+  }
+
+  /**
+   * Creates a feature group containing all markers created from elements.
+   * @private
+   */
+  private _createMarkersLayer(): FeatureGroup {
+    return featureGroup(this._cleanedElements.map(el => el.marker));
+  }
+
+  /**
+   * Creates a feature group containing all circles created from elements.
+   * @private
+   */
+  private _createCirclesLayer(): FeatureGroup {
+    return featureGroup(this._cleanedElements.map(el => {
+      const circle = el.circle;
+
+      if (!this.randomizeCircle) {
+        return circle;
+      }
+
+      const location = CoordsConverter.latLngToGeopoint(circle.getLatLng());
+      const randomLocation = this.mapHelper.randomizeCircleLocation(location, el.id, circle.getRadius());
+      return cloneLayer(circle).setLatLng(CoordsConverter.geopointToLatLng(randomLocation));
+    }));
+  }
+
+  /**
+   * Update fitBounds on map.
+   * This method assumes _leafletMap and _markersLayer are not undefined.
+   * @private
+   */
+  private _updateFitBounds() {
+    if (this._leafletMap && this._markersLayer.getLayers().length > 1) {
+      this._leafletMap.fitBounds(this._markersLayer.getBounds());
+    } else if (this._markersLayer.getLayers().length === 1) {
+      this._leafletMap.setView(this._markersLayer.getBounds().getCenter(), this.zoom);
     }
+  }
 
-    /**
-     * Method called when map is ready to be used.
-     * @param map Map object ready to be used.
-     */
-    mapReady(map: Map): void {
-        this._leafletMap = map;
-
-        this._updateFitBounds();
+  private _refreshMap() {
+    if (this._leafletMap) {
+      this._leafletMap.invalidateSize();
+      this._updateFitBounds();
     }
+  }
 
-
-    /*
-     *   ___     _          _
-     *  | _ \_ _(_)_ ____ _| |_ ___
-     *  |  _/ '_| \ V / _` |  _/ -_)
-     *  |_| |_| |_|\_/\__,_|\__\___|
-     *
-     */
-
-    get _cleanedElements(): MapElementInput[] {
-        if (!this.elements) {
-            return [];
-        }
-
-        this.elements = this.elements.filter(el => !!el);
-
-        return this.elements;
+  private _showOrHideCurrentPositionButton() {
+    if (this._leafletMap) {
+      if (this.showCurrentPositionButton) {
+        this._currentPositionButton.addTo(this._leafletMap);
+      } else {
+        this._currentPositionButton.remove();
+      }
     }
-
-    get _markersLayer(): FeatureGroup {
-        return this._createMarkersLayer();
-    }
-
-    get _circlesLayer(): FeatureGroup {
-        return this._createCirclesLayer();
-    }
-
-    /**
-     * Creates a feature group containing all markers created from elements.
-     * @private
-     */
-    private _createMarkersLayer(): FeatureGroup {
-        return featureGroup(this._cleanedElements.map(el => el.marker));
-    }
-
-    /**
-     * Creates a feature group containing all circles created from elements.
-     * @private
-     */
-    private _createCirclesLayer(): FeatureGroup {
-        return featureGroup(this._cleanedElements.map(el => {
-            const circle = el.circle;
-
-            if (!this.randomizeCircle) {
-                return circle;
-            }
-
-            const location = CoordsConverter.latLngToGeopoint(circle.getLatLng());
-            const randomLocation = this.mapHelper.randomizeCircleLocation(location, el.id, circle.getRadius());
-            return cloneLayer(circle).setLatLng(CoordsConverter.geopointToLatLng(randomLocation));
-        }));
-    }
-
-    /**
-     * Update fitBounds on map.
-     * This method assumes _leafletMap and _markersLayer are not undefined.
-     * @private
-     */
-    private _updateFitBounds() {
-        if (this._leafletMap && this._markersLayer.getLayers().length > 1) {
-            this._leafletMap.fitBounds(this._markersLayer.getBounds());
-        } else if (this._markersLayer.getLayers().length === 1) {
-            this._leafletMap.setView(this._markersLayer.getBounds().getCenter(), this.zoom);
-        }
-    }
-
-    private _refreshMap() {
-        if (this._leafletMap) {
-            this._leafletMap.invalidateSize();
-            this._updateFitBounds();
-        }
-    }
+  }
 }
