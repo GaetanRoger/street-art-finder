d005b8f381502683e8eada34ef85da86577e0bfc
Gaetan Roger <gaetanroger.fr@gmail.com>
2019-01-12 22:11:45 +0100

    Upgrade & better UI

--- src/app/modules/shared/components/flat-card-with-image/flat-card-with-image.component.ts
+++ src/app/modules/shared/components/flat-card-with-image/flat-card-with-image.component.ts
@@ -2,36 +2,35 @@ import {Component, Input, OnChanges} from '@angular/core';
 import {DomSanitizer, SafeValue} from '@angular/platform-browser';
 
 @Component({
-    selector: 'streart-flat-card-with-image',
-    templateUrl: './flat-card-with-image.component.html',
-    styleUrls: ['./flat-card-with-image.component.scss']
+  selector: 'streart-flat-card-with-image',
+  templateUrl: './flat-card-with-image.component.html',
+  styleUrls: ['./flat-card-with-image.component.scss']
 })
 export class FlatCardWithImageComponent implements OnChanges {
-    @Input() title: string;
-    @Input() titleClass = '';
-    @Input() text: string;
-    @Input() image: string;
-    @Input() primaryButtonText: string;
-    @Input() secondaryButtonText: string;
-    @Input() primaryButtonRouterLink: string;
-    @Input() secondaryButtonRouterLink: string;
-    @Input() primaryButtonExternalLink: string;
-    @Input() secondaryButtonExternalLink: string;
+  @Input() title: string;
+  @Input() titleClass = '';
+  @Input() text: string;
+  @Input() image: string;
+  @Input() primaryButtonText: string;
+  @Input() secondaryButtonText: string;
+  @Input() primaryButtonRouterLink: string;
+  @Input() secondaryButtonRouterLink: string;
+  @Input() primaryButtonExternalLink: string;
+  @Input() secondaryButtonExternalLink: string;
 
-    backgroundImageProperty: SafeValue;
+  backgroundImageProperty: SafeValue;
 
+  constructor(private readonly sanitizer: DomSanitizer) {
+  }
 
-    constructor(private readonly sanitizer: DomSanitizer) {
+  ngOnChanges() {
+    if (!this.image) {
+      return;
     }
 
-    ngOnChanges() {
-        if (!this.image) {
-            return;
-        }
-
-        this.backgroundImageProperty = this.sanitizer.bypassSecurityTrustStyle(
-            `background-image: url("${this.image}");`
-        );
-    }
+    this.backgroundImageProperty = this.sanitizer.bypassSecurityTrustStyle(
+      `background-image: url("${this.image}");`
+    );
+  }
 
 }
